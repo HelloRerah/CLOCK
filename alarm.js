e82c6826f3ad83f3e08f9028629cdd810b40aecd
HelloRerah <reratilwetompanerati@gmail.com>
2025-05-20 12:43:43 +0200

    Add files via upload

---
+++ alarm.js
@@ -0,0 +1,6 @@
+//console.log("Hello World");
+document.getElementById("heading").textContent= "HELLO AGAIN!";
+let a; //declaration
+a= 3;//assignment
+x=23; //declaration and assignment 
+console.log(`you are ${x} years old!`);
---
+++ home.js
@@ -0,0 +1,3 @@
+console.log("HELLO WORLD");
+console.log("okay it's working now");
+window.alert("This is an alert!");
